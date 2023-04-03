3d3ccb62805b620248b51b28f18e91a6592a28bd
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-04-03 13:50:48 +0530

    Update Nav.jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/home">Home</Link>
           </li>
           <li>
             <Link to="/products">Products</Link>
