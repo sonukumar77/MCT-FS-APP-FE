4d3ce4b1f600143ffa6d3d73e3f70ca5f3d3b045
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-04-03 11:34:47 +0530

    Update ProductDetails.jsx

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -8,38 +8,44 @@ const ProductDetails = () => {
   const [product, setProduct] = useState({});
   //   const { cat, _id } = useParams();
   const { _id } = useParams();
+  const  [isLoading,setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchCategory = async () => {
+      setIsLoading(true);
       const res = await fetch(`${BASE_URL}/product/${_id}`);
       const data = await res.json();
       setProduct(data.product);
+      setIsLoading(false);
     };
     fetchCategory();
   }, [_id]);
 
   return (
     <div className="prduct-details-container">
-      <div className="card-wrapper">
-        <header>
-          <h1>{product?.title}</h1>
-        </header>
-        <div className="card">
-          <div className="card-header">
-            <img src={product.image} alt="product" />
-          </div>
-          <div className="card-body">
-            <h4>Product Name</h4>
-            <p>{product?.title}</p>
-            <h4>Product Price</h4>
-            <p>$ {product?.price}.00</p>
-            <h4>Product Description</h4>
-            <p>{product?.description}</p>
-            <h4>Product Rating</h4>
-            <p>{product?.rating?.rate}</p>
-          </div>
-        </div>
-      </div>
+         {isLoading?<p style={{fontSize:"2rem"}}>Loading...</p>:(
+                <div className="card-wrapper">
+                <header>
+                  <h1>{product?.title}</h1>
+                </header>
+                <div className="card">
+                  <div className="card-header">
+                    <img src={product.image} alt="product" />
+                  </div>
+                  <div className="card-body">
+                    <h4>Product Name</h4>
+                    <p>{product?.title}</p>
+                    <h4>Product Price</h4>
+                    <p>$ {product?.price}.00</p>
+                    <h4>Product Description</h4>
+                    <p>{product?.description}</p>
+                    <h4>Product Rating</h4>
+                    <p>{product?.rating?.rate}</p>
+                  </div>
+                </div>
+              </div>
+         )}
+
     </div>
   );
 };
